6f5fb9d052239e5942862ac4cb3059dfc55ee397
DenisStenklerTrailer <77147548+DenisStenklerTrailer@users.noreply.github.com>
2023-02-08 18:39:22 +0100

    Getting temperature and sky data from the API

--- backend.py
+++ backend.py
@@ -2,11 +2,19 @@ import requests
 
 API_key = "141710af2113bab9f55ef73e1bcd33d5"
 
-def get_data(place, days=None, kind=None):
+def get_data(place, days, kind):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_key}"
     response = requests.get(url)
     content = response.json()
-    return content
+    filtered_content = content['list']
+    filtered_content = filtered_content[:8 * days]
+
+    if kind == "Temperature":
+        filtered_content = [temp['main']['temp'] for temp in filtered_content]
+    elif kind == "Sky":
+        filtered_content = [sky['weather'][0]['main'] for sky in filtered_content]
+
+    return filtered_content
 
 if __name__ == "__main__":
-    print(get_data(place="Koritnica"))
+    print(get_data(place="Koritnica", days=2, kind="Sky"))
